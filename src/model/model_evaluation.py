84cbaad2ff3fc2a9cb17558d445486f6c9f7e225
hanit_kaur_cw <hanitkaut14@gmail.com>
2025-05-14 03:02:34 +0530

    sending all updated dvc pipeline

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -150,10 +150,10 @@ def main():
             mlflow.log_artifact('reports/metrics.json')
 
             # Log the model info file to MLflow
-            #mlflow.log_artifact('reports/model_info.json')
+            mlflow.log_artifact('reports/model_info.json')
 
             # Log the evaluation errors log file to MLflow
-           # mlflow.log_artifact('model_evaluation_errors.log')
+            mlflow.log_artifact('model_evaluation_errors.log')
         except Exception as e:
             logger.error('Failed to complete the model evaluation process: %s', e)
             print(f"Error: {e}")

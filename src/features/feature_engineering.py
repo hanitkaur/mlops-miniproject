6cb075e7cff9f8e3c2b95595b256f90d80f4a445
hanit_kaur_cw <hanitkaut14@gmail.com>
2025-05-14 19:28:44 +0530

    Feature engineering updated

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -74,7 +74,9 @@ def apply_bow(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: i
         test_df = pd.DataFrame(X_test_bow.toarray())
         test_df['label'] = y_test
 
-        pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb'))
+        os.makedirs('models', exist_ok=True)
+        with open('models/vectorizer.pkl', 'wb') as f:
+            pickle.dump(vectorizer, f)
 
 
 
